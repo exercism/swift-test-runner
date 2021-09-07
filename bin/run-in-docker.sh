b25bfc385870be18dc0ad25801ce6b95aa4a9e21
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-07 14:27:57 +0200

    Update docker settings

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -63,6 +63,9 @@ mkdir -p "$OUTPUT_DIR"
 
 # run image passing the arguments
 docker run \
-    --mount type=bind,src=${INPUT_DIR},dst=/solution \
-    --mount type=bind,src=${OUTPUT_DIR},dst=/output \
+    --network none \
+    --read-only \
+    --mount type=bind,src="${INPUT_DIR}",dst=/solution \
+    --mount type=bind,src="${OUTPUT_DIR}",dst=/output \
+    --mount type=volume,dst=/tmp \
     exercism/swift-test-runner $SLUG /solution/ /output/
