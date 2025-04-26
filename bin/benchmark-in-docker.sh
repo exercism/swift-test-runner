02ac80f4114b8a57ca70a96cba6c0d7578f53c9b
meatball <69751659+meatball133@users.noreply.github.com>
2025-04-26 17:22:48 +0000

    Changes based on feedback

--- bin/benchmark-in-docker.sh
+++ bin/benchmark-in-docker.sh
@@ -26,14 +26,14 @@ required_tool docker
 required_tool hyperfine
 
 # Pre-build the Docker image
-if [ -z "${SKIP_DOCKER_BUILD}" ]; then
+if [[ -z "${SKIP_DOCKER_BUILD}" ]]; then  
   docker build --rm -t exercism/replace-this-with-the-track-slug-test-runner .
 else
   echo "Skipping docker build because SKIP_DOCKER_BUILD is set."
 fi
 
-CURRENT_PATH=$(pwd)
+CURRENT_PATH=${PWD}
 
 hyperfine \
-    --parameter-list slug $(find tests -maxdepth 1 -mindepth 1 -type d -printf $'%f\n' | paste -sd ",") \
+    --parameter-list slug $(find tests -maxdepth 1 -mindepth 1 -type d -printf '%f\n' | paste -sd ",") \ 
     "SKIP_DOCKER_BUILD=true bin/run-in-docker.sh {slug} $CURRENT_PATH/tests/{slug} $CURRENT_PATH/tests/{slug}" \
